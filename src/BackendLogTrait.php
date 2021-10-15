04082633c9718dad8eccc65800c1c1a08402594e
Yusuf Gandhi Putra <gandhi.putra@gmail.com>
2021-10-15 10:24:34 +0700

    new backend log trait

--- src/BackendLogTrait.php
+++ src/BackendLogTrait.php
@@ -9,7 +9,7 @@ trait BackendLogTrait
 {
     public function logInfo(string $message)
     {
-        return $message;
+        return $message . ". And this is from the class itself";
         // $logger = new Logger('test logger');
         // $logger->pushHandler(new StreamHandler("php://stdout"));
         // $logger->info($message);
